6580be394aa71adc6762fff9958c834dc2e375c4
JJ <jj.vega86@gmail.com>
2021-03-02 20:13:27 -0500

    Added initial GET axios request to search for videos

--- src/app.js
+++ src/app.js
@@ -1,21 +1,25 @@
 import React, { Component } from "react";
 import SearchBar from "./components/SearchBar/SearchBar";
+import youtube from "./api/youtube";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {data: ''};
+    this.state = { data: "" };
   }
 
-  onSearchSubmit(){
-      //TODO: complete logic after integrating YouTube API
-
-  }
+  onSearchSubmit = async (term) => {
+    await youtube.get(`/search`, {
+      params: {
+        q: term,
+      },
+    });
+  };
 
   render() {
     return (
       <div className="ui container">
-        <SearchBar onSubmit={this.onSearchSubmit}/>
+        <SearchBar onSubmit={this.onSearchSubmit} />
       </div>
     );
   }
