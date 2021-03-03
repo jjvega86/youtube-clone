d8302d02ffbdf651c7f794d56c975888fa5cd7d3
JJ <jj.vega86@gmail.com>
2021-03-03 08:36:59 -0500

    Added Semantic UI Grid system to the app component to get VideoList to display to the right of VideoDetail

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import SearchBar from "./components/SearchBar/SearchBar";
 import youtube from "./api/youtube";
 import VideoList from "./components/VideoList/VideoList";
-import VideoDetail from './components/VideoDetail/VideoDetail';
+import VideoDetail from "./components/VideoDetail/VideoDetail";
 
 class App extends Component {
   constructor(props) {
@@ -31,12 +31,21 @@ class App extends Component {
     return (
       <div className="ui container">
         <SearchBar onSubmit={this.onSearchSubmit} />
-        <div className="ui hidden divider"></div> {/* Creates easy margin between components. Could also write CSS */}
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList
-          onVideoSelect={this.onVideoSelect}
-          videos={this.state.videos}
-        />
+        <div className="ui hidden divider"></div>{" "}
+        {/* Creates easy margin between components. Could also write CSS */}
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
