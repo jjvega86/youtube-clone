bcb55e21efe77979cead55529f4832cdef30e1dd
JJ <jj.vega86@gmail.com>
2021-03-02 20:01:44 -0500

    Added default configurations for axios object in youtube.js

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,1 +1,12 @@
-const KEY = 'AIzaSyDoj19NPebJQAWyWZm-4jCrD9pmsB3Qvd4'
+import axios from "axios";
+
+const KEY = "AIzaSyDoj19NPebJQAWyWZm-4jCrD9pmsB3Qvd4";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY,
+  },
+});
